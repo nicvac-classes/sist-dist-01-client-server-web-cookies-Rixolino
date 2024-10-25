7d91e4aee53c5d8530c49efba066e2b53ad41903
Simone Risola #19 <71252421+Rixolino@users.noreply.github.com>
2024-10-25 12:10:45 +0000

    CONSEGNA

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const cookieParser = require('cookie-parser');
 
 // Configurazione EJS
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
-    res.render('form');
+    const name = req.cookies.name;
+    if (name) {
+        res.render('greet', {message:'Bentornato',name:name});
+    } else {
+        res.render('form');   
+    }
 });
 
 app.post('/greet', (req, res) => {
     const name = req.body.name;
-    res.render('greet', { name: name });
+    res.cookie('name',name, {maxAge: 24*60*60*1000});
+    res.render('greet', { message: 'Benvenuto',name: name });
+});
+
+app.post('/logout', (req,res) => {
+    const name = req.body.name;
+    res.clearCookie('name');
+    res.redirect('/')
 });
 
 const PORT = 3000;
